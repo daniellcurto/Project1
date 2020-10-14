c047b75140829698ed6d31c9c2a9318035651527
daniellcurto <70148832+daniellcurto@users.noreply.github.com>
2020-10-14 18:23:57 -0400

    Update scripts.sh

--- Script/scripts.sh
+++ Script/scripts.sh
@@ -56,13 +56,14 @@ printHelp() {
   if [[$1 = '-help'] || [$1 = 'help'] || [$1 = '-h'] || [$1 = 'h']]; then
     echo "This script will set itself up in the .profile file."
     echo "It will disable the '98-reboot-required' and the '90-updates-available' files"
-    echo "It will also add a custom motd with the saying Welcome to your system '$(echo $USER)'. There are currently '$(find . | wc -l)'-nodes running on your account'  and will add it to the curto-motd file"
+    echo "It will also add a custom motd with the saying Welcome to your system '$(echo $USER)'. There are currently '$(find . | wc -l)'-nodes running on your account.' This motd will be addded to the 'curto-motd file'."
     echo "The script will make sure the file 'curto-motd' will given the same permissions and ownership as the other motd files"
-    echo "The Solarized vim color scheme and the desired custom adjustments will be aded to .profile"
+    echo "The script can also download the Solarized vim color scheme and set all the desired custom adjustments that the writer desires"
     echo "The first alias called 'aws-ssh' will point to ssh -i demo.key ubuntu@54.205.113.32"
     echo "The second alias called 'moo' will point to sudo apt-get install cowsay"
     echo "This help option can be called by -help or help or -h or h"
     echo "Also, there is a random motd that can be installed from the internet called fortune-mod cowsay"
+    echo "If there are any typos or errors please contact Daniel Curto at curto.2@wright.edu to correct any mistakes"
     # call printHelp
 fi
 }
